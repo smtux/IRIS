1daa11ec15f002a15a44c496d08653a99460b58f
smtux <sm@solhefa.fr>
2023-05-13 01:17:37 +0200

    Mise à jour de la branche filtre

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import iris
 
 # chargement du dataset
-iris_data = pd.read_csv(iris.csv, header=None)
+iris_data = pd.read_csv("iris.csv", header=None)
 
 print("\n### Affichage des 5 premieres lignes du dataset")
 print(iris_data.head())
