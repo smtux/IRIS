76a81ed6b71d50874be57023459726d1302e3f23
smtux <sm@solhefa.fr>
2023-05-13 01:09:09 +0200

    Création de la fonction de création de graphiques

--- barplot.py
+++ barplot.py
@@ -1,7 +1,19 @@
-# Production d'un graph de type scatter plot
 import matplotlib.pyplot as plt
 
-# calcul de la moyenne par nom d'iris
 def iris_mean(dataset):
-	return df = dataset.groupby(4).mean()
-
+	df = dataset.groupby(5).mean().dropna()
+	df.drop(0, axis=1, inplace=True)
+	print(df)
+	#x = [1, 2, 3, 4]
+	color = ["red", "blue", "green"]
+	x = ["sepal length", "sepal width", "petal length", "petal width"]
+	fig, ax = plt.subplots(len(df.index), 1, figsize=(10,7), sharex=True)
+	i = 0
+	for name, data in df.iterrows():
+		ax[i].bar(x, data, color=color[i])
+		ax[i].set_title("Plant: " + name)
+		ax[i].set_ylabel("Mean")
+		i += 1
+	ax[i-1].set_xlabel("Attribute")
+	fig.subplots_adjust(hspace=1)
+	plt.show()
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pandas as pd
+import barplot
 
 # chargement du dataset
-iris_data = pd.read_csv(iris.csv, header=None)
+iris_data = pd.read_csv("iris.csv", header=None)
 
 print("\n### Affichage des 5 premieres lignes du dataset")
-print(iris_data.head())
+print(iris_data.head(10))
 
 print("\n### affichage des variables ###")
 print(iris_data.info())
@@ -13,6 +14,7 @@ print("\n### calculs stats du dataset##")
 print(iris_data.describe())
 
 print("\n### comptage des valeurs regroupées par nom ###")
-print(iris_data.groupby(4).count())
+print(iris_data.groupby(5).count())
 
-iris.filtre(iris_data, "Iris-setosa")
+print("### bar plot ###")
+print(barplot.iris_mean(iris_data))
